7b9d45c3f85a83f578bf0602275aad5614005aca
Ilham Robbani <robbani.ilham2002@gmail.com>
2023-09-29 23:47:06 +0700

    update logo

--- resources/views/layouts/side-navbar.blade.php
+++ resources/views/layouts/side-navbar.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel = "icon" href ="{{ asset('img/logo GMF.png') }}" type = "image/x-icon">
   <title>GMF X UNPAD | {{$title}} </title>
   
   <!-- Tailwind is included -->
